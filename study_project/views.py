10700b6d36ab9d88c0f889d5330caaaaf29f1bc3
TomNgo03 <ngoquangtung281203@gmail.com>
2023-06-20 21:38:59 +0700

    Update login and register

--- study_project/views.py
+++ study_project/views.py
@@ -8,3 +8,50 @@ from .models import Room, Topic, Message, User
 from .forms import RoomForm, UserForm, MyUserCreationForm
 
 # Create your views here.
+
+
+def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated():
+        return redirect('home')
+    
+    if request.method == 'POST':
+        email = request.POST.get('email').lower()
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(email=email)
+        except:
+            message.error(request, 'User does not exist')
+        
+        user = authenticate(request, email=email, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password is incorrect')
+        
+    context = {'page': page}
+    return render(request, 'study_project/login_register.html', context)
+
+def logoutPage(request):
+    logout(request)
+    return redirect('home')
+
+def registerPage(request):
+    form = MyUserCreationForm()
+    
+    if request.method == 'POST':
+        form = MyUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred while registering')
+    
+    return render(request, 'study_project/login_register.html', {'form': form})
+

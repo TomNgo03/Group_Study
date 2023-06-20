f48b4366bf764686fc005e183eb4d79234368f07
TomNgo03 <ngoquangtung281203@gmail.com>
2023-06-20 22:16:52 +0700

    Update CRUD for room

--- study_project/views.py
+++ study_project/views.py
@@ -8,8 +8,6 @@ from .models import Room, Topic, Message, User
 from .forms import RoomForm, UserForm, MyUserCreationForm
 
 # Create your views here.
-
-
 def loginPage(request):
     page = 'login'
     if request.user.is_authenticated():
@@ -54,4 +52,99 @@ def registerPage(request):
             messages.error(request, 'An error occurred while registering')
     
     return render(request, 'study_project/login_register.html', {'form': form})
+
+def home(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    
+    rooms = Rooms.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+    
+    topics = Topic.objects.all()[0:10]
+    room_count = rooms.count()
+    rooms_messages = Message.objects.filter(
+        Q(room__topic__name__icontains=q)[0:5]
+    )
+    
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'rooms_messages': rooms_messages}
+    
+    return render(request, 'study_project/home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all()
+    participants = room.participants.all()
+    
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return render(request, 'study_project/room.html', context)
+    
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    rooms_messages = user.message_set.all()
+    topics = Topics.objects.all()
+    context = {'user': user, 'rooms': rooms, 'rooms_messages': rooms_messages, 'topics': topics}
+    
+    return render(request, 'study_project/profile.html', context)
+
+@login_required(login_url='login')
+def createRoom(request):
+    form = RoomForm()
+    topics = Topic.objects.all()
+    if request.method == 'POST':
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name = topic_name)
+        
+        Room.objects.create(
+            host = request.user
+            topic = topic
+            name = request.POST.get('name')
+            description = request.POST.get('description')
+        )
+        return redirect('home')
+    
+    context = {'form': form, 'topics': topics}
+    return render(request, 'study_project/room_form.html', context)
+
+@login_required(login_url='login')
+def updateRoom(request, pk):
+    room = Room.objects.get(id = pk)
+    form = RoomForm(instance = room)
+    topics = Topic.objects.all()
+    
+    if request.user != room.host:
+        return HttpResponse('You are not allowed to update this room')
+    
+    if request.method == 'POST':
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name = topic_name)
+        room.name = request.POST.get('name')
+        room.description = request.POST.get('description')
+        room.topic = topic
+        room.save()
+        return redirect('home')
+    
+    context = {'form': form, 'topics': topics, 'room': room}
+    return render(request, 'study_project/room_form.html', context)
+
+@login_required(login_url='login')
+def deleteRoom(request, pk):
+    room = Room.objects.get(id = pk)
+    
+    if request.user != room.host:
+        return HttpResponse('You are not allowed to delete this room')
+
+    if request.method = 'POST':
+        room.delete()
+        return redirect('home')
+    
+    return render(request, 'study_project/delete.html', {'obj': room})
 

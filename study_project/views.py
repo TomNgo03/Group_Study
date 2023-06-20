7eec99c4a07ca67b616b9d7dafc50a4c415fe33e
TomNgo03 <ngoquangtung281203@gmail.com>
2023-06-20 17:41:08 +0700

    Update html and css

--- study_project/views.py
+++ study_project/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.contrib.auth import authenticate, login, logout
+from .models import Room, Topic, Message, User
+from .forms import RoomForm, UserForm, MyUserCreationForm
 
 # Create your views here.

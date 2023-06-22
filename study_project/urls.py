db84d48442971472fdd7ddc95e74ed74974a490e
TomNgo03 <ngoquangtung281203@gmail.com>
2023-06-22 21:23:24 +0700

    Update OpenAI api

--- study_project/urls.py
+++ study_project/urls.py
@@ -19,4 +19,5 @@ urlpatterns = [
 
     path('topics/', views.topicsPage, name="topics"),
     path('activity/', views.activityPage, name="activity"),
+    path('chat/', views.chatPage, name="chat"),
 ]
--- study_project/views.py
+++ study_project/views.py
@@ -6,6 +6,12 @@ from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic, Message, User
 from .forms import RoomForm, UserForm, MyUserCreationForm
+import openai, os
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = os.getenv('OPENAI_KEY', None)
+
 
 # Create your views here.
 def loginPage(request):
@@ -194,3 +200,23 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
         
     return render(request, 'study_project/update-user.html', {'form': form})
+
+@login_required(login_url='login')
+def chatPage(request):
+    chatbot_response = None
+    if api_key is not None and request.method == 'POST':
+        openai.api_key = api_key
+        user_input = request.POST.get('user_input')
+        prompt = user_input
+        
+        response = openai.Completion.create(
+            engine = 'text-davinci-003',
+            prompt = prompt,
+            max_tokens = 256,
+            # stop = ".",
+            temperature = 0.5,
+        )
+        print(response)
+        chatbot_response = response['choices'][0]['text']
+    return render(request, 'study_project/chat.html', {})
+

24caf8c92c68a1a024026c9caf6bf3ea92b613ee
TomNgo03 <ngoquangtung281203@gmail.com>
2023-06-26 00:32:41 +0700

    Admin register Task

--- study_project/admin.py
+++ study_project/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Room, Topic, Message, User
+from .models import Room, Topic, Message, User, Task
 
 admin.site.register(User)
 admin.site.register(Room)
 admin.site.register(Topic)
 admin.site.register(Message)
+admin.site.register(Task)

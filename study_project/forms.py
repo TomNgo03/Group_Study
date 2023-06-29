b506f29e766b9b14f95dea2c0ea76bdefd186dda
TomNgo03 <ngoquangtung281203@gmail.com>
2023-06-29 13:02:52 +0700

    Update task form

--- study_project/forms.py
+++ study_project/forms.py
@@ -22,33 +22,3 @@ class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'reminder_option', 'reminder_day', 'reminder_date', 'reminder_month']
-
-# class TaskForm(ModelForm):
-#     class Meta:
-#         model = Task
-#         fields = '__all__'
-#         exclude = ['created']
-
-#     def clean(self):
-#         cleaned_data = super().clean()
-#         reminder_option = cleaned_data.get('reminder_option')
-
-#         if reminder_option == 'weekly':
-#             reminder_day = cleaned_data.get('reminder_day')
-#             if reminder_day is None:
-#                 self.add_error('reminder_day', 'Please select a day of the week.')
-
-#         if reminder_option == 'monthly':
-#             reminder_date = cleaned_data.get('reminder_date')
-#             if reminder_date is None:
-#                 self.add_error('reminder_date', 'Please enter a date of the month.')
-
-#         if reminder_option == 'yearly':
-#             reminder_month = cleaned_data.get('reminder_month')
-#             reminder_date = cleaned_data.get('reminder_date')
-#             if reminder_month is None:
-#                 self.add_error('reminder_month', 'Please select a month.')
-#             if reminder_date is None:
-#                 self.add_error('reminder_date', 'Please enter a date.')
-
-#         return cleaned_data

416d275108d49f3498963082334eeddddde7d5c8
TomNgo03 <ngoquangtung281203@gmail.com>
2023-07-03 11:41:54 +0700

    Update Calendar

--- study_project/models.py
+++ study_project/models.py
@@ -6,6 +6,8 @@ class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
+    google_access_token = models.CharField(max_length=255, blank=True, null=True)
+
 
     avatar = models.ImageField(null=True, default="avatar.svg")
     

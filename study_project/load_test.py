fe755bbb5f88dedeb9babeb3f09e288af55ecff6
TomNgo03 <ngoquangtung281203@gmail.com>
2023-07-18 21:44:00 +0700

    Performance tests for room and task view

--- study_project/load_test.py
+++ study_project/load_test.py
@@ -1,1 +1,34 @@
 from locust import HttpUser, between, task
+
+class MyUser(HttpUser):
+    wait_time = between(1, 2)
+    
+    @task
+    def view_task_list(self):
+        self.client.get("/task_list")
+        
+    def create_task(self):
+        task_data = {
+            'title': 'Test Task',
+            'description': 'Test Task Description',
+        }
+        self.client.post('/create-task/', json=task_data)
+        
+class MyUser(HttpUser):
+    wait_time = between(1, 2) 
+
+    @task
+    def view_room(self):
+        self.client.get('/room/1/')
+        
+    @task
+    def update_room(self):
+        room_data = {
+            'name': 'Updated Room',
+            'description': 'Updated Room Description',
+        }
+        self.client.post('/update-room/1/', json=room_data)
+    
+    @task
+    def delete_room(self):
+        self.client.post('/delete-room/1/') 
